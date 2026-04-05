6ac98432d6931eddcd1fdc7962c846ee7c2f8a55
Vikas <vikask4590@gmail.com>
2026-04-05 23:16:46 +0530

    deployment: 05-04-2026 23:16

--- getPrice.js
+++ getPrice.js
@@ -98,30 +98,30 @@ const getPrices = async () => {
     });
 
     // Start
-    prices.forEach((p) => {
-      if (p.symbol === "gold") {
-        const baseSymbol = p.symbol === "gold" ? "goldnext" : "silvernext";
-        const basePrice = prices.find((bp) => bp.symbol === baseSymbol);
-        if (basePrice) {
-          p.Bid = basePrice.Bid;
-          p.Ask = basePrice.Ask;
-          p.High = basePrice.High;
-          p.Low = basePrice.Low;
-          p.LTP = basePrice.LTP;
-          p.Rate = basePrice.Rate;
-          p.Direction = basePrice.Direction;
-          p.BidDirection = basePrice.BidDirection;
-          p.AskDirection = basePrice.AskDirection;
-          p.Difference = basePrice.Difference;
-          p.RateDifference = basePrice.RateDifference;
-          p.BidDifference = basePrice.BidDifference;
-          p.AskDifference = basePrice.AskDifference;
-          p.BidDifferencePercentage = basePrice.BidDifferencePercentage;
-          p.AskDifferencePercentage = basePrice.AskDifferencePercentage;
-          p.RateDifferencePercentage = basePrice.RateDifferencePercentage;
-        }
-      }
-    });
+    // prices.forEach((p) => {
+    //   if (p.symbol === "gold") {
+    //     const baseSymbol = p.symbol === "gold" ? "goldnext" : "silvernext";
+    //     const basePrice = prices.find((bp) => bp.symbol === baseSymbol);
+    //     if (basePrice) {
+    //       p.Bid = basePrice.Bid;
+    //       p.Ask = basePrice.Ask;
+    //       p.High = basePrice.High;
+    //       p.Low = basePrice.Low;
+    //       p.LTP = basePrice.LTP;
+    //       p.Rate = basePrice.Rate;
+    //       p.Direction = basePrice.Direction;
+    //       p.BidDirection = basePrice.BidDirection;
+    //       p.AskDirection = basePrice.AskDirection;
+    //       p.Difference = basePrice.Difference;
+    //       p.RateDifference = basePrice.RateDifference;
+    //       p.BidDifference = basePrice.BidDifference;
+    //       p.AskDifference = basePrice.AskDifference;
+    //       p.BidDifferencePercentage = basePrice.BidDifferencePercentage;
+    //       p.AskDifferencePercentage = basePrice.AskDifferencePercentage;
+    //       p.RateDifferencePercentage = basePrice.RateDifferencePercentage;
+    //     }
+    //   }
+    // });
     // End
 
     prices.forEach((p) => {
